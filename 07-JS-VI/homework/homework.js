f819494fa58079bdc4cc257cf015344a6c6a8eef
yusdalismaria <yusdalismmg@gmail.com>
2021-12-15 18:03:35 -0400

    hola soy tarea nueva cambiada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -61,13 +61,20 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var nuevoArray = [];
+  /*var nuevoArray = [];
   for(let i = 0; i<array.length; i++) {
     if(array[i][0] === "a") {
       nuevoArray.push(array[i])
     }
   }
   return nuevoArray;
+*/
+
+  var nuevoArreglo= array.filter(function(elemento){
+    return elemento [0]==='a';
+  });
+return nuevoArreglo;
+
 }
 
 // No modificar nada debajo de esta línea
